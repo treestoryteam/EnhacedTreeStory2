be25f6d3b4a654a4b844c3ce74e89b357833ba8e
David W <DavidRubioLucas@gmail.com>
2016-07-17 14:01:08 +0200

    Introducidos cambios en navbar

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module TreeStories
     #Añadido
     #Añadido mejora
     config.exceptions_app = self.routes
-
+    config.i18n.default_locale = :es
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -34,7 +34,7 @@ module TreeStories
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    #config.i18n.default_locale = :es
+
     #
   end
 end
